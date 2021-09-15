df39719e14e24120a5400e3eb72288bf9393cb8a
Abhishek <abhishekb042@gmail.com>
2021-09-15 22:35:49 +0530

    Added length_two_line is equal or not code

--- src/Calculate_length_two_points.java
+++ src/Calculate_length_two_points.java
@@ -3,20 +3,40 @@ import java.util.Scanner;
 public class Calculate_length_two_points {
 	
 	static void getVal(){
-		
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the value of x1: ");  
-		double x1 = input.nextDouble();  
-		System.out.print("Enter the value of x2: ");  
-		double x2 = input.nextDouble();  
-		System.out.print("Enter the value of y1: ");  
-		double y1 = input.nextDouble();
-		System.out.print("Enter the value of y2: ");  
-		double y2 = input.nextDouble();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Value of x1: ");
+		double x1 = sc.nextDouble();
+		System.out.println("Value of x2: ");
+		double x2 = sc.nextDouble();
+		System.out.println("Value of y1: ");
+		double y1 = sc.nextDouble();
+		System.out.println("Value of y2: ");
+		double y2 = sc.nextDouble();
+
 		double a = x2 - x1;
-        double b = y2 - y1;
-        double length_of_line = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-        System.out.println("Length of Line : " + length_of_line);
+		double b = y2 - y1;
+
+		System.out.println("Value of m1: ");
+		double m1 = sc.nextDouble();
+		System.out.println("Value of m2: ");
+		double m2 = sc.nextDouble();
+		System.out.println("Value of n1: ");
+		double n1 = sc.nextDouble();
+		System.out.println("Value of n2: ");
+		double n2 = sc.nextDouble();
+
+		double p = m2 - m1;
+		double q = n2 - n1;
+
+		double length_of_line1 = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+		double length_of_line2 = Math.sqrt(Math.pow(p, 2) + Math.pow(q, 2));
+
+		if (length_of_line1 == length_of_line2) {
+			System.out.println("length of both line is equal");
+		} else {
+			System.out.println("length of both line is not equal");
+		}
 
 }
 }	
